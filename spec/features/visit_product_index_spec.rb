cc6be69c55a00353ac2608225bfd1c0b9abfc33a
Max <max@Maxs-Mac-mini.local>
2015-09-21 03:03:47 +0300

    Develoe product index page design

--- spec/features/visit_product_index_spec.rb
+++ spec/features/visit_product_index_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.feature 'Visit product index' do
   let!(:products) { create_list(:product, 3) }
 
-  scenario 'User visits product index' do
+  scenario 'Guest visits product index' do
     visit products_path
 
     expect(page).to have_selector('h1', text: 'Products')
 
     products.each do |product|
-      expect(page).to have_selector('h2', text: product.name)
+      expect(page).to have_content product.name
       expect(page).to have_css("img[src=\"#{product.photo.url}\"]")
       expect(page).to have_css("a[href=\"#{product_path(product)}\"]")
     end
