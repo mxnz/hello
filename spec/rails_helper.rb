61c67391e09ce05b38145b2863ab6f1c445fbb2f
Max <max@Maxs-Mac-mini.local>
2015-09-27 02:44:51 +0300

    Add AdminMailer

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'pundit/rspec'
+require 'capybara/email/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -61,4 +62,5 @@ RSpec.configure do |config|
   config.include Rails.application.routes.url_helpers
   config.include Capybara::DSL
   config.include FeaturesMacros, type: :feature
+  config.include Capybara::Email::DSL, type: :mailer
 end
